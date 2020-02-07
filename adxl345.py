6ff0d23726d59f9f714ea9032001e1fa0d1ce08b
juan benjumea <jubenjum@gmail.com>
2020-02-07 15:15:17 +0100

    dump data into an open socket

--- adxl345.py
+++ adxl345.py
@@ -1,17 +1,37 @@
 from machine import I2C, RTC, Timer
 import time
+import pycom
 import struct
 import array
+
+try:
+    import usocket as socket
+except:
+    import socket
+
+pycom.heartbeat(False)
+
+HOST = '192.168.4.2'
+PORT = 80
+
 
 class ADXL345():
     def __init__(self, file_, dt=1.0):
         self.f = open(file_, 'w')
         self.dt = dt
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.s.connect((HOST, PORT))
+            self.socket_ = True
+        except:
+            print('no connection to {}'.format(HOST))
+            self.socket_ = False
 
         self.n = 0
         self.buf_index = 0
-        self.buf_0_len = 2
-        self.buf_0 = [[0, 0] for i in range(self.buf_0_len)]
+        self.buf_0_len = 1
+        self.buf_0 = [[0, 0, 0] for i in range(self.buf_0_len)]
 
         self.rtc = RTC()
         self.i2c = I2C()
@@ -31,31 +51,42 @@ class ADXL345():
         self.get_data()
 
         self.buf_0[self.buf_index][0] = time.ticks_diff(self.begin, time.ticks_us())
-        self.buf_0[self.buf_index][1] = self.data
+        self.buf_0[self.buf_index][1] = struct.unpack('<hhh', self.data)
+        self.buf_0[self.buf_index][2] = struct.pack('l', time.ticks_diff(self.begin, time.ticks_us()))
 
         self.buf_index += 1
         if self.buf_index >= self.buf_0_len:
             self.write_to_file()
             self.buf_index = 0
 
+    def format_buf(self, i):
+        return b"{} {} {} {}\n".format(self.buf_0[i][0],\
+            self.buf_0[i][1][0], self.buf_0[i][1][1], self.buf_0[i][1][2])
+
     def write_to_file(self):
         for i in range(len(self.buf_0)):
-            self.f.write("{} {}\n".format(self.buf_0[i][0],
-                struct.unpack('<hhh', self.buf_0[i][1])))
+            if self.socket_:
+                self.s.sendall(self.format_buf(i))
+            else: # save to a file
+                self.f.write(self.format_buf(i))
 
     def stop(self):
         self.__alarm.callback(None)
         print("n=%d"%self.n)
         for i in range(self.buf_index):
-            self.f.write("{} {}\n".format(self.buf_0[i][0],
-                struct.unpack('<hhh', self.buf_0[i][1])))
+            if self.socket_:
+                self.s.sendall(self.format_buf(i))
+            else: # save to a file
+                self.f.write(self.format_buf(i))
 
         self.f.close()
+        if self.socket_:
+            self.s.close()
 
     def get_data(self):
         self.data = self.i2c.readfrom_mem(83, 0x32, 6)
 
 
-s = ADXL345('data.txt', 0.005)
-time.sleep(600)
+s = ADXL345('data.txt', 0.01)
+time.sleep(10)
 s.stop()
